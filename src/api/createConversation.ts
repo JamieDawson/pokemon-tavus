faa72ac61860f24004bd88aa03e1c74ea86a2bd6
JamieDawson <jamierobertdawson@gmail.com>
2025-06-14 17:45:35 -0700

    updated replica. Removed looping BG footage of Santa

---
+++ src/api/createConversation.ts
@@ -0,0 +1,25 @@
+import { IConversation } from "@/types";
+
+export const createConversation = async (
+  token: string,
+): Promise<IConversation> => {
+  const response = await fetch("https://tavusapi.com/v2/conversations", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": token ?? "",
+    },
+    body: JSON.stringify({
+      // Santa Demo Persona
+      replica_id: "rb17cf590e15",
+      persona_id: "p3bb4745d4f9",
+    }),
+  });
+
+  if (!response?.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
